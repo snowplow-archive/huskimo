d09641df429479d0cf337fd159b0dbed89754c47
dennisatspaceape <dennis.waldron@spaceapegames.com>
2015-10-14 15:35:24 +0100

    Added missing singular_creative values and optimized Redshift column encodings

--- sql/singular-redshift-ddl.sql
+++ sql/singular-redshift-ddl.sql
@@ -18,23 +18,23 @@
 
 -- Create table for Singular campaigns
 CREATE TABLE huskimo.singular_campaigns (
-  channel_name        varchar(512) not null encode runlength,
-  when_retrieved      timestamp not null encode runlength,
-  ad_network          varchar(512) not null encode text32k,
-  campaign_name       varchar(512) not null encode text32k,
-  campaign_type       varchar(512) not null encode text32k,
-  campaign_url        varchar(8192),
-  subcampaign_name    varchar(512) encode text32k,
-  app_id              varchar(512) encode runlength,
-  campaign_network_id varchar(512) encode runlength,
-  country             varchar(2) encode text255,
-  reporting_date      timestamp not null encode runlength, -- Renamed from date to standardize
-  impressions         bigint,
-  clicks              bigint,
-  installs            bigint,
-  cost                decimal(10, 2),
-  revenue             decimal(10, 2),
-  last_modified       timestamp not null
+  channel_name        VARCHAR(512) NOT NULL ENCODE RUNLENGTH,
+  when_retrieved      TIMESTAMP    NOT NULL ENCODE LZO,
+  ad_network          VARCHAR(512) NOT NULL ENCODE LZO,
+  campaign_name       VARCHAR(512) NOT NULL ENCODE LZO,
+  campaign_type       VARCHAR(512) NOT NULL ENCODE LZO,
+  campaign_url        VARCHAR(8192)         ENCODE LZO,
+  subcampaign_name    VARCHAR(512)          ENCODE LZO,
+  app_id              VARCHAR(512)          ENCODE LZO,
+  campaign_network_id VARCHAR(512)          ENCODE LZO, -- Always empty
+  country             VARCHAR(2)            ENCODE LZO,
+  reporting_date      TIMESTAMP NOT NULL    ENCODE LZO, -- Renamed from date$
+  impressions         BIGINT                ENCODE LZO,
+  clicks              BIGINT                ENCODE LZO,
+  installs            BIGINT                ENCODE LZO,
+  cost                DECIMAL(10, 2)        ENCODE LZO,
+  revenue             DECIMAL(10, 2)        ENCODE LZO,
+  last_modified       TIMESTAMP NOT NULL    ENCODE LZO
 )
 DISTSTYLE KEY
 DISTKEY (ad_network)
@@ -42,28 +42,30 @@ SORTKEY (reporting_date, when_retrieved);
 
 -- Create table for Singular creatives
 CREATE TABLE huskimo.singular_creatives (
-  channel_name        varchar(512) not null encode runlength,
-  when_retrieved      timestamp not null encode runlength,
-  ad_network          varchar(512) not null encode text32k,
-  creative_name       varchar(512) encode text32k,
-  creative_text       varchar(512) encode text32k,
-  image               varchar(8192) not null encode text32k,
-  image_hash          varchar(512) not null encode text32k,
-  width               varchar(512) encode text32k,
-  height              varchar(512) encode text32k,
-  is_video            boolean not null encode runlength,
-  campaign_name       varchar(512) not null encode text32k,
-  campaign_type       varchar(512) not null encode text32k,
-  campaign_url        varchar(8192),
-  subcampaign_name    varchar(512) encode text32k,
-  app_id              varchar(512) encode runlength,
-  campaign_network_id varchar(512) encode runlength,
-  reporting_date      timestamp not null encode runlength, -- Renamed from date to standardize
-  impressions         bigint,
-  clicks              bigint,
-  installs            bigint,
-  cost                decimal(10, 2),
-  modified_at         timestamp not null
+  channel_name        VARCHAR(512)  NOT NULL ENCODE RUNLENGTH,
+  when_retrieved      TIMESTAMP     NOT NULL ENCODE LZO,
+  ad_network          VARCHAR(512)  NOT NULL ENCODE LZO,
+  creative_name       VARCHAR(512)           ENCODE LZO,
+  creative_text       VARCHAR(512)           ENCODE LZO,
+  creative_url        VARCHAR(8192)          ENCODE LZO,
+  creative_network_id VARCHAR(512)           ENCODE LZO,
+  image               VARCHAR(8192) NOT NULL ENCODE LZO,
+  image_hash          VARCHAR(512)  NOT NULL ENCODE LZO,
+  width               INTEGER                ENCODE LZO,
+  height              INTEGER                ENCODE LZO,
+  is_video            BOOLEAN       NOT NULL ENCODE RAW,
+  campaign_name       VARCHAR(512)  NOT NULL ENCODE LZO,
+  campaign_type       VARCHAR(512)  NOT NULL ENCODE LZO,
+  campaign_url        VARCHAR(8192)          ENCODE LZO,
+  subcampaign_name    VARCHAR(512)           ENCODE LZO,
+  app_id              VARCHAR(512)           ENCODE LZO,
+  campaign_network_id VARCHAR(512)           ENCODE LZO,
+  reporting_date      TIMESTAMP     NOT NULL ENCODE LZO, -- Renamed from date$
+  impressions         BIGINT                 ENCODE LZO,
+  clicks              BIGINT                 ENCODE LZO,
+  installs            BIGINT                 ENCODE LZO,
+  cost                DECIMAL(10, 2)         ENCODE LZO,
+  modified_at         TIMESTAMP NOT NULL     ENCODE LZO
 )
 DISTSTYLE KEY
 DISTKEY (ad_network)
